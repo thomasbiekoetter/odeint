758f01d8eddff395e85be14943a5ab441dceda85
Thomas <thomas.biekoetter@desy.de>
2026-06-08 15:15:28 +0200

    Changed terminal print of test programs.

--- test/rk4.f90
+++ test/rk4.f90
@@ -14,7 +14,12 @@ program odeint__test_rk4
 
   call integrate(exp_decay, y0, tstart, tend, nsteps, t, y)
 
-  write(*,*) t(nsteps), y(nsteps, :)
+  write(*, '(A)')         "RK4 integration: dy/dt = -y,  y(0) = 1"
+  write(*, '(A, I0)')     "  steps     : ", nsteps
+  write(*, '(A, F6.2)')   "  t         : ", t(nsteps)
+  write(*, '(A, ES12.5)') "  y(t)      : ", y(nsteps, 1)
+  write(*, '(A, ES12.5)') "  exact     : ", exp(-t(nsteps))
+  write(*, '(A, ES12.5)') "  error     : ", abs(y(nsteps, 1) - exp(-t(nsteps)))
 
 contains
 
